2532269885b040d7cef4fe8457e7432290e04440
amitbhange <52119796+amitbhange@users.noreply.github.com>
2021-06-01 13:59:04 +0530

    Update UserDetails.java

--- app/src/main/java/com/example/dukandar/UserDetails.java
+++ app/src/main/java/com/example/dukandar/UserDetails.java
@@ -7,60 +7,46 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 
 public class UserDetails extends AppCompatActivity {
     private Button button7;
-    EditText name,address,mobNo;
+    private EditText name, address, mobNo;
     FirebaseDatabase rootNode;
-    DatabaseReference reference,dbref;
-
+    DatabaseReference reference;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_details);
 
-       name =(EditText) findViewById(R.id.name);
-       address = (EditText) findViewById(R.id.address);
-       mobNo = (EditText)findViewById(R.id.mobNo);
-       button7=(Button) findViewById(R.id.button7);
+        name = findViewById(R.id.name);
+        address =findViewById(R.id.address);
+        mobNo =findViewById(R.id.mobNo);
+        button7 =findViewById(R.id.button7);
 
-      reference = FirebaseDatabase.getInstance().getReference().child("UserDetails");
-
-       button7.setOnClickListener(new View.OnClickListener(){
+        button7.setOnClickListener(new View.OnClickListener(){
             @Override
-            public void onClick(View view){
+            public void onClick(View view) {
 
+                rootNode = FirebaseDatabase.getInstance();
+                reference= rootNode.getReference("orders");
 
-                 dbref=reference.child("UserDetails");
-                final String uniqueKey=dbref.push().getKey();
+                String name1=name.getText().toString();
+                String MobNo1 = mobNo.getEditableText().toString();
+                String address1=address.getEditableText().toString();
 
-                UserHelperClass helperClass=new UserHelperClass( name.getText().toString() , mobNo.getText().toString(),address.getText().toString(),uniqueKey);
-                dbref.child(uniqueKey).setValue(helperClass).addOnSuccessListener(new OnSuccessListener<Void>() {
-                    @Override
-                    public void onSuccess(Void aVoid) {
-                        Toast.makeText(UserDetails.this, "Success", Toast.LENGTH_SHORT).show();
-                    }
+                UserHelperClass hp=new UserHelperClass(name1,MobNo1,address1);
 
-                }).addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Toast.makeText(UserDetails.this, "Something went wrong", Toast.LENGTH_SHORT).show();
-                    }
-                });
+                reference.push().setValue(hp);
 
-
-                Toast toast = Toast.makeText(getApplicationContext(), "Simple Toast In Android", Toast.LENGTH_LONG); // initiate the Toast with context, message and duration for the Toast
-              toast.show(); // display the Toast
             }
-
         });
+
 
     }
 }
